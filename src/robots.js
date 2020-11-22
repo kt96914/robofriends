fe910c3db9b22038c98088873be1542fce5dd479
Keshaw Thakur <keshavthakur676@gmail.com>
2020-11-22 21:06:50 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots =[
+ {
+     id: 1,
+     name: 'Rafeh Qazi',
+     username: 'Clever',
+     email: 'qazi@yahoo.com'
+ },
+
+{
+    id:2,
+    name: 'Neil Broom',
+    username: 'Antonette',
+    email: 'shopify@gmail.com'
+},
+
+{
+    id:3,
+    name: 'Sunny Deol',
+    username: 'Sam',
+    email: 'sam2001@gmail.com'
+},
+
+{
+    id:4,
+    name: 'Roger Fernando',
+    username: 'Machine',
+    email: 'test@gmail.com'
+},
+
+{
+    id:5,
+    name: 'Astuti Thakur',
+    username: 'Allen',
+    email: 'astutithakur2004@gmail.com'
+},
+
+{
+    id:6,
+    name: 'Raabindra Thakur',
+    username: 'Ravi',
+    email: 'ravithakur2004@gmail.com'
+},
+
+{
+    id:7,
+    name: 'Amita Thakur',
+    username: 'Ash',
+    email: 'ashthakur2004@gmail.com'
+},
+
+{
+    id:8,
+    name: 'Gregory dsouza',
+    username: 'Hero',
+    email: 'greggoyal2004@gmail.com'
+},
+
+]
